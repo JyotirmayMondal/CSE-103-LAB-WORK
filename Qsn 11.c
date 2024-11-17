3f0c0556a5bd568c7d62008996cb53b6ed402722
Jyotirmay_05 <jfreelancing007@gmail.com>
2024-11-17 09:28:22 -0800

    Add files via upload

--- Qsn 11.c
+++ Qsn 11.c
@@ -5,36 +5,32 @@
 //if taxable income is above 5,00,001 then charge 30% tax.
 
 #include<stdio.h>
-
-
 int main(){
 float income , tax , grossincome ;
 printf("Input Income : ");
 scanf("%f",&income);
-
 if(income<=150000){
         printf("No Tax added\n");
         printf("Persons Income is : %.2f",income);
 }
 else if(income>=150001 && 300000>=income){
-        tax=income*0.1;
+        tax=(income-150000)*0.1;
         printf("10 percent Tax added:%f\n",tax);
         grossincome=income-tax;
         printf("Persons Income is : %.2f",grossincome);
 }
 else if(income>=300001 && 500000>=income){
-        tax=income*0.2;
+        tax=(150000*0.1)+(income-300000)*0.2;
         printf("20 percent Tax added:%f\n",tax);
         grossincome=income-tax;
         printf("Persons Income is : %.2f",grossincome);
 }
 else if(income>=500001){
-        tax=income*0.3;
+        tax=(150000*0.1)+(200000*0.2)+((income-500000)*0.3);
         printf("30 percent Tax added:%f\n",tax);
         grossincome=income-tax;
         printf("Persons Income is : %.2f",grossincome);
 }
-
-
 return 0;
 }
+
